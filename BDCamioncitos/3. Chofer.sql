4778fd04f5e47c7dca290931aa2b5c96ddfce4cc
CesarFCL <57689841+CesarFCL@users.noreply.github.com>
2022-08-06 11:32:23 -0500

    Versión 0.6

--- BDCamioncitos/3. Chofer.sql
+++ BDCamioncitos/3. Chofer.sql
@@ -31,6 +31,28 @@ begin
 end
 go
 
+-- Procedimiento para Obtener Choferes Disponibles
+CREATE PROC ObtenerChoferDisponible
+@Condicion nvarchar(30)
+as
+begin
+	SET NOCOUNT ON
+	SELECT
+		E.CI as 'Cedula',
+		E.NOMBRE as 'Nombre',
+		E.APELLIDO as 'Apellido',
+		E.CELULAR as 'Celular',
+		E.FECHA_NACIMIENTO as 'Fecha de Nacimiento',
+		E.CORREO as 'Correo',
+		E.DIRECCION as 'Direccion',
+		CASE WHEN D_C.DISPONIBILIDAD = 1 then 'Disponible' else 'No disponible' end as 'Disponibilidad'
+
+		FROM EMPLEADOS E
+		JOIN DISPONIBILIDAD_CHOFER D_C ON E.CI = D_C.CI
+		where D_C.DISPONIBILIDAD=1 and (E.CI like @Condicion+'%' or E.NOMBRE like @Condicion+'%' or E.APELLIDO like @Condicion+'%')
+end
+go
+
 --Procedimiento para Modificar Chofer
 CREATE PROC ModificarChofer
 @CI  VARCHAR(10),
@@ -53,6 +75,19 @@ BEGIN
 			CORREO = @CORREO,
 			DIRECCION = @DIRECCION
      WHERE  CI = @CI
+	 UPDATE DISPONIBILIDAD_CHOFER
+	 SET	DISPONIBILIDAD = CASE WHEN @DISPONIBILIDAD = 'Disponible' then 1 else 0 end
+	 WHERE	CI = @CI
+END 
+go
+
+--Procedimiento para Modificar Disponibilidad Chofer
+CREATE PROC ModificarDisponibilidadChofer
+@CI  VARCHAR(10),
+@DISPONIBILIDAD VARCHAR(15)
+as
+BEGIN 
+     SET NOCOUNT ON 
 	 UPDATE DISPONIBILIDAD_CHOFER
 	 SET	DISPONIBILIDAD = CASE WHEN @DISPONIBILIDAD = 'Disponible' then 1 else 0 end
 	 WHERE	CI = @CI
--- BDCamioncitos/5. Vehiculo.sql
+++ BDCamioncitos/5. Vehiculo.sql
@@ -40,6 +40,91 @@ create table TIPOS_VEHICULOS
 );
 go
 
+-- Tabla Chofer_Vehiculo
+create table CHOFER_VEHICULO
+(
+  CI_CHOFER VARCHAR(10) primary key,
+  MATRICULA_VEHICULO VARCHAR(10) not null
+  CONSTRAINT fk_MATRICULA_CV FOREIGN KEY (MATRICULA_VEHICULO) REFERENCES VEHICULO (MATRICULA),
+  CONSTRAINT fk_CI_CV FOREIGN KEY (CI_CHOFER) REFERENCES EMPLEADOS (CI)
+  ON UPDATE cascade
+  ON DELETE cascade
+);
+go
+
+--Procedimiento para Obtener Choferes Vinculados a Vehiculo
+CREATE PROC ObtenerVinculoChoferVehiculo
+@Condicion nvarchar(30)
+as
+begin
+	SET NOCOUNT ON
+	SELECT
+		CI_CHOFER as 'CEDULA CHOFER',
+		MATRICULA_VEHICULO as 'MATRICULA VEHICULO'
+		FROM CHOFER_VEHICULO
+		where CI_CHOFER like @Condicion+'%' or MATRICULA_VEHICULO like @Condicion+'%'
+end
+go
+
+--Procedimiento para crear Union Chofer_Vehiculo 
+CREATE PROC UnirChoferVehiculo
+@CI_CHOFER VARCHAR(10),
+@MATRICULA_VEHICULO VARCHAR(10)
+as
+begin
+	if exists
+	(
+		select CI
+		from EMPLEADOS_CARGOS
+		where CI = @CI_CHOFER and ID_CARGO = '00C'
+	)
+	begin
+		SET NOCOUNT ON
+		insert into CHOFER_VEHICULO values(@CI_CHOFER,@MATRICULA_VEHICULO);
+	end
+	if exists
+	(
+		select CI_CHOFER
+		from CHOFER_VEHICULO
+		where CI_CHOFER = @CI_CHOFER
+	)
+	begin
+		SET NOCOUNT ON
+		UPDATE DISPONIBILIDAD_VEHICULO
+		 SET	DISPONIBILIDAD = 0
+		 WHERE	MATRICULA = @MATRICULA_VEHICULO;
+		UPDATE DISPONIBILIDAD_CHOFER
+		 SET	DISPONIBILIDAD = 0
+		 WHERE	CI = @CI_CHOFER;
+	 end
+end
+go
+
+--Procedimiento para eliminar Union Chofer_Vehiculo (falta implementar)
+CREATE PROC DesvincularChoferVehiculo
+@CI_CHOFER VARCHAR(10),
+@MATRICULA_VEHICULO VARCHAR(10)
+as
+begin
+	if exists
+	(
+		select CI_CHOFER, MATRICULA_VEHICULO
+		from CHOFER_VEHICULO
+		where CI_CHOFER = @CI_CHOFER and MATRICULA_VEHICULO = @MATRICULA_VEHICULO
+	)
+	begin
+	SET NOCOUNT ON
+	delete from CHOFER_VEHICULO where CI_CHOFER = @CI_CHOFER;
+	UPDATE DISPONIBILIDAD_VEHICULO
+		 SET	DISPONIBILIDAD = 1
+		 WHERE	MATRICULA = @MATRICULA_VEHICULO;
+	UPDATE DISPONIBILIDAD_CHOFER
+		 SET	DISPONIBILIDAD = 1
+		 WHERE	CI = @CI_CHOFER;
+	end
+end
+go
+
 -- Procedimiento para Obtener Vehiculo
 CREATE PROC ObtenerVehiculo
 @Condicion nvarchar(30)
@@ -61,7 +146,28 @@ begin
 end
 go
 
---Procedimiento para Crear VEHICULO
+-- Procedimiento para Obtener Vehiculos Disponibles
+CREATE PROC ObtenerVehiculoDisponible
+@Condicion nvarchar(30)
+as
+begin
+	SET NOCOUNT ON
+	SELECT
+		V.MATRICULA as 'Matricula',
+		V.MARCA as 'Marca',
+		V.AÑO as 'Año',
+		L_T_V.NOMBRE as 'Tipo',
+		CASE WHEN D_V.DISPONIBILIDAD = 1 then 'Disponible' else 'No disponible' end as 'Disponibilidad'
+
+		FROM VEHICULO V
+		JOIN DISPONIBILIDAD_VEHICULO D_V ON V.MATRICULA = D_V.MATRICULA
+		JOIN TIPOS_VEHICULOS T_V ON V.MATRICULA = T_V.MATRICULA
+		JOIN LISTADO_TIPOS_VEHICULOS L_T_V ON T_V.ID_TIPO_VEHICULO = L_T_V.ID_TIPO_VEHICULO
+		where D_V.DISPONIBILIDAD = 1 and (V.MATRICULA like @Condicion+'%' or L_T_V.NOMBRE like @Condicion+'%' or V.MARCA like @Condicion+'%')
+end
+go
+
+--Procedimiento para Crear Vehiculo
 CREATE PROC CrearVehiculo
 @MATRICULA  VARCHAR(10),
 @MARCA VARCHAR(50),
@@ -76,7 +182,7 @@ begin
 end
 go
 
---Procedimiento para Eliminar Cliente
+--Procedimiento para Eliminar Vehiculo
 CREATE PROC EliminarVehiculo
 @MATRICULA  VARCHAR(10)
 as
@@ -86,7 +192,7 @@ begin
 end
 go
 
---Procedimiento para Modificar Cliente
+--Procedimiento para Modificar Vehiculo
 CREATE PROC ModificarVehiculo
 @MATRICULA  VARCHAR(10),
 @MARCA VARCHAR(50),
